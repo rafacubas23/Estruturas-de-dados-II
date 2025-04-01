417343d1f33c74dbe9eabfba03e16509b1d2aa8c
Administrador <rafael.cubas.work@gmail.com>
2025-04-01 13:47:49 -0300

    Questão 5 Feita

---
+++ Simulado/questoes_5-8/questao_5.py
@@ -0,0 +1,12 @@
+def ver_produtos():
+    f = open("PRODUTOS.TXT", "r")
+    for linha in f:
+        linha = linha.strip()
+
+        codigo = linha[:4].ljust(4)
+        nome_produto = linha[4:19].ljust(15)
+        preco = linha[19:].rjust(5)
+
+        print(f"Código: {codigo} | Produto: {nome_produto} | Preço: {preco}")
+
+ver_produtos()
